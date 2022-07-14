5d48f78671ecf0cc1b585a6d26f776abdb88dd04
David Polanía <david.polania3@gmail.com>
2022-07-14 03:56:20 -0500

    diagonals in progress. Falta estrella central

--- script-logic/main.go
+++ script-logic/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"strings"
 
@@ -8,7 +9,7 @@ import (
 )
 
 func main() {
-	dnaRequest := []string{"ATGCGA", "CAGTGC", "TTATGT", "AGAGGG", "CCCCTA", "TCACTG"}
+	dnaRequest := []string{"ATGCGA","CAGTGC","TTATGT","AGAAGG","CCCCTA","TCACTG"}
 
 	//Convert slices to string
 	dnaString := strings.Join(dnaRequest, "")
@@ -26,27 +27,30 @@ func main() {
 //Outputs: true if there is an anomaly pattern in the dna sequence
 func checkIfMutant(dna [][]string) bool {
 	count := 0
-	count += checkHorizontal(dna)
+	/*count = checkHorizontal(dna, count)
 	if count > 1 {
 		return true
 	}
-	count += checkVertical(dna)
+	count += checkVertical(dna, count)
+	if count > 1 {
+		return true
+	}*/
+	count += checkDiagonalNegative(dna)
 	if count > 1 {
 		return true
 	}
 	count += checkDiagonalPositive(dna)
-	if count > 1 {
-		return true
-	}
-	count += checkDiagonalNegative(dna)
 	return count > 1
 }
 
-func checkHorizontal(dna [][]string) int {
-	//fmt.Println("[Matrix]", dna)
-	count := 0
+func checkHorizontal(dna [][]string, count int) int {
+	fmt.Println("[Matrix]:", dna)
 
 	for i := 0; i < len(dna); i++ {
+		//Si en esta secuencia count es mayor que 1 se retorna el valor de count
+		if count > 1 {
+			break
+		}
 		//search hints for possible patterns
 		if dna[i][0] == dna[i][2] {
 			if dna[i][0] == dna[i][1] && dna[i][1] == dna[i][3] {
@@ -65,14 +69,45 @@ func checkHorizontal(dna [][]string) int {
 	return count
 }
 
-func checkVertical(dna [][]string) int {
+func checkVertical(dna [][]string, count int) int {
 	dnaMatrix := utils.TransposeMatrix(dna)
-	count := checkHorizontal(dnaMatrix)
+	count = checkHorizontal(dnaMatrix, count)
 	return count
 }
 
 func checkDiagonalNegative(dna [][]string) int {
+	log.Println("[dna]: ", dna)
 	count := 0
+
+	//1 step: Check diagonal
+	if dna[0][0] == dna[3][3]{
+		if dna[1][1] == dna[2][2]{
+			count++
+		}
+	}else if dna[3][3] == dna[4][4]{
+		if dna[2][2] == dna[5][5]{
+			count++
+		}
+
+		adjX1 := 0
+		adjX2 := 3
+		centerX:=3
+		centerY:=2
+	//2 step, check adjacent diagonals
+	for i := 1; i <= 2; i++ {
+		if dna[adjX1][i] == dna[adjX2][i+adjX2]{
+			if dna[adjX1+1][i+1] == dna[adjX2-1][i+2]{
+				count++
+			}else if i == 1{
+				if dna[centerX][centerY] == dna[centerX+1][centerY+1]{
+					count++
+				}
+			}	
+		}
+	}
+	//3 step, check middle 
+	}
+	
 	return count
 }
 
