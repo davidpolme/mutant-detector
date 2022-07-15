a7d04ad7dcad1ea3f22a50be375b17f07911665e
David Polanía <david.polania3@gmail.com>
2022-07-15 13:40:44 -0500

    feat: finished Algorithm

--- script-logic/main.go
+++ script-logic/main.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	dnaRequest := []string{"ATGCGA","CAGTGC","TTATGT","AGAAGG","CCCCTA","TCACTG"}
+	dnaRequest := []string{"TCACTG", "TTGCGA", "CAGTGC", "TTATGT", "AGAAGG", "CCCCAA"}
 
 	//Convert slices to string
 	dnaString := strings.Join(dnaRequest, "")
@@ -75,39 +75,81 @@ func checkVertical(dna [][]string, count int) int {
 	return count
 }
 
+func checkMainDiagonal(dna [][]string, count int) int {
+	if count > 1 {
+		return count
+	}
+	if dna[0][0] == dna[3][3] {
+		if dna[1][1] == dna[2][2] {
+			count++
+		}
+	} else if dna[3][3] == dna[4][4] {
+		if dna[2][2] == dna[5][5] {
+			count++
+		}
+	}
+	return count
+}
+
+func checkAdjacentDiagonal(dna [][]string, count int) int {
+	if count > 1 {
+		return count
+	}
+
+	adjX1 := 0
+	adjX2 := 3
+	centerX := 2
+	centerY := 3
+
+	for i := 1; i <= 2; i++ {
+		//Check left diagonals
+		if dna[adjX1][i] == dna[adjX2][i+adjX2] {
+			if dna[adjX1+1][i+1] == dna[adjX2-1][i+2] {
+				count++
+			}
+		} else if i == 1 {
+			//Its necesary to check both of possibilities in the nearest diagonal from  the main diagonal. When i = 1
+			if dna[centerX][centerY] == dna[centerX+1][centerY+1] {
+				if dna[centerX-1][centerY-1] == dna[centerX+2][centerY+2] {
+					count++
+				}
+			}
+		}
+	}
+
+	if count > 1 {
+		return count
+	}
+
+	for i := 1; i <= 2; i++ {
+		//Check left diagonals
+		if dna[i][adjX1] == dna[i+adjX2][adjX2] {
+			if dna[i+1][adjX1+1] == dna[i+2][adjX2-1] {
+				count++
+			}
+		} else if i == 1 {
+			//Its necesary to check both of possibilities in the nearest diagonal from  the main diagonal. When i = 1
+			if dna[centerY][centerX] == dna[centerY+1][centerX+1] {
+				if dna[centerY-1][centerX-1] == dna[centerY+2][centerX+2] {
+					count++
+				}
+			}
+		}
+	}
+	return count
+}
+
 func checkDiagonalNegative(dna [][]string) int {
 	log.Println("[dna]: ", dna)
 	count := 0
 
-	//1 step: Check diagonal
-	if dna[0][0] == dna[3][3]{
-		if dna[1][1] == dna[2][2]{
-			count++
-		}
-	}else if dna[3][3] == dna[4][4]{
-		if dna[2][2] == dna[5][5]{
-			count++
-		}
+	//1 step: Check main diagonal
+	count = checkMainDiagonal(dna, count)
 
-		adjX1 := 0
-		adjX2 := 3
-		centerX:=3
-		centerY:=2
 	//2 step, check adjacent diagonals
-	for i := 1; i <= 2; i++ {
-		if dna[adjX1][i] == dna[adjX2][i+adjX2]{
-			if dna[adjX1+1][i+1] == dna[adjX2-1][i+2]{
-				count++
-			}else if i == 1{
-				if dna[centerX][centerY] == dna[centerX+1][centerY+1]{
-					count++
-				}
-			}	
-		}
-	}
-	//3 step, check middle 
-	}
-	
+	count = checkAdjacentDiagonal(dna, count)
+
+	log.Println("[count]: ", count)
 	return count
 }
 
