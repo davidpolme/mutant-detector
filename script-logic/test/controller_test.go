f318423c5ab95824750fc6e7f53a2b9032c31683
David Polanía <david.polania3@gmail.com>
2022-07-17 20:25:40 -0500

    test

--- script-logic/test/controller_test.go
+++ script-logic/test/controller_test.go
@@ -1,23 +1,20 @@
-package main
+package tests
 
 import (
-	"log"
-	"strings"
+	"testing"
 
 	"github.com/davidpolme/mutant-detector/script-logic/controllers"
 	"github.com/davidpolme/mutant-detector/script-logic/utils"
 )
 
-func main() {
+func TestCheckMutant(t *testing.T) {
 	dnaRequest := []string{"TCACTG", "TTGCGA", "CAGTGC", "TTATGT", "AGAAGG", "CCCCAA"}
-
-	//Convert slices to string
-	dnaString := strings.Join(dnaRequest, "")
-	//Separate string into slices of strings
-	dna := utils.StringToSlice(dnaString, 6)
 	//Convert slices to matrix
-	dnaMatrix := utils.SliceToMatrix(dna)
+	dnaMatrix := utils.SliceToMatrix(dnaRequest)
 	//Check if there is a pattern
+
 	dnaResponse := controllers.CheckIfMutant(dnaMatrix)
-	log.Println("[result]:", dnaResponse)
+	if !dnaResponse {
+		t.Errorf("Expected true, got %v", dnaResponse)
+	}
 }
